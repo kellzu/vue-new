ecb8dda52268d8c4aaa3609e5b8cb4f774dfe2c3
Kfoot <1070248289@qq.com>
2018-11-17 14:35:53 +0800

    第一次提交

---
+++ js/index.js
@@ -0,0 +1,10 @@
+/*
+* @Author: Kelly
+* @Date:   2018-11-16 14:38:38
+* @Last Modified by:   Kelly
+* @Last Modified time: 2018-11-17 09:33:37
+*/
+
+$('.lookMore').on('tap',function(){
+  alert("hhh")
+})
